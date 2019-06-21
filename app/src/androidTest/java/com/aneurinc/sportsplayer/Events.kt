a2429bab2ef825d42a6fe7d3b0f64f4098e4834a
Nye <nsc9012@gmail.com>
2019-06-21 11:54:42 -0600

    Implemented android tests.

---
+++ app/src/androidTest/java/com/aneurinc/sportsplayer/Events.kt
@@ -0,0 +1,13 @@
+
+package com.aneurinc.sportsplayer
+
+import android.support.annotation.IdRes
+import android.support.test.espresso.Espresso.onView
+import android.support.test.espresso.action.ViewActions.click
+import android.support.test.espresso.matcher.ViewMatchers.withId
+
+class Events {
+    fun clickOnView(@IdRes viewId: Int) {
+        onView(withId(viewId)).perform(click())
+    }
+}
